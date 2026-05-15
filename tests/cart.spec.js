a95151ce26bbd57246318e4fab98e22091b309ac
ryan23nobrega <ryannobregarc23@gmail.com>
2026-05-15 11:03:18 -0300

    refatoração: usar fixture de usuário no teste de carrinho

--- tests/cart.spec.js
+++ tests/cart.spec.js
@@ -1,43 +1,29 @@
 const { test, expect } = require('@playwright/test');
-
 const LoginPage = require('../pages/LoginPage');
 const InventoryPage = require('../pages/InventoryPage');
+const users = require('../fixtures/userData');
 
 let inventoryPage;
 
 test.beforeEach(async ({ page }) => {
-
   const loginPage = new LoginPage(page);
-
   inventoryPage = new InventoryPage(page);
-
   await loginPage.accessWebsite();
-
   await loginPage.login(
-    'standard_user',
-    'secret_sauce'
+    users.standardUser.username,
+    users.standardUser.password
   );
-
 });
 
 test('Adicionar produto ao carrinho', async ({ page }) => {
-
   await inventoryPage.addBackpackToCart();
-
   await inventoryPage.accessCart();
-
   await expect(page.locator('.cart_item')).toBeVisible();
-
 });
 
 test('Remover produto do carrinho', async ({ page }) => {
-
   await inventoryPage.addBackpackToCart();
-
   await inventoryPage.accessCart();
-
   await inventoryPage.removeBackpack();
-
   await expect(page.locator('.cart_item')).toHaveCount(0);
-
 });
